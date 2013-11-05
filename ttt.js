961260f256b598101cecd4cdde179b682212b89c
Nathan White <nw@nwhite.net>
2013-11-04 20:17:19 -0700

    basic function

--- ttt.js
+++ ttt.js
@@ -2,10 +2,28 @@ $(function(){
   
   var table = $('table');
   var board = [];
+  var players = [ 'x', 'o' ];
+  var current = 0;
+  var msg = $('#msg');
+  var whos_turn = $('#whos_turn');
 
   createTable(table);
+  reset();
 
+  $('td').on('click', function(){
+    var el = $(this);
+    
+    if(el.text() !== '') return msg.text('already set');
+    
+    el.text(players[current]);
+    
+    current = (current < players.length-1) ? current + 1 : 0;
+    
+    whos_turn.text('player '+ (current+1));
+    
+  });
   
+  $('#reset').on('click', reset);
 
   function createTable(el){
     times(3, function(ridx){
@@ -30,6 +48,13 @@ $(function(){
     });
     
   }
+  
+  function reset(){
+      $('td').text('');
+      current = 0;
+      msg.text('');
+      whos_turn.text('player '+ (current+1));
+    }
 
 
 })
