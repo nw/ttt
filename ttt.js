7f781e8842cb07fd62921883a00d3c29e8d253c2
log0s <JewishRhino@gmail.com>
2013-11-04 20:09:53 -0700

    Added function to check win conditions Added check object to contain functions that check for win conditions being accomplished. Contains five functions: winCondition, a helper function used by the other functions, allConditions, a function that checks the overall win status of a player, and vertical, diagonal, and horizontal, which check the win conditions of their respective types

--- ttt.js
+++ ttt.js
@@ -1,6 +1,6 @@
 'use strict';
-var player1 = { active: true },
-    player2 = { active: false },
+var player1 = { active: true, win: false },
+    player2 = { active: false, win: false },
     savedPieces = [[],[],[]],
     $board = $('table'),
     
@@ -19,6 +19,7 @@ var player1 = { active: true },
             app.updatePieces();
             player1.active = !player1.active;
             player2.active = !player2.active;
+            app.check.allConditions();
         }
     },
     
@@ -34,7 +35,42 @@ var player1 = { active: true },
         $('td').each(function() {
             $(this).html('');
         });
-    }          
+        
+        app.updatePieces();
+        player1.active = true;
+        player1.active = false;
+    },
+    
+    check: {
+        winCondition: function(piece, params) {
+            return ( (savedPieces[params.p1x][params.p1y] === piece) && (savedPieces[params.p2x][params.p2y] === piece) && (savedPieces[params.p3x][params.p3y] === piece) );
+        },
+        
+        horizontal: function(piece) {
+            var params = [ {p1x:0, p1y:0, p2x:0, p2y:1, p3x:0, p3y:2}, {p1x:1, p1y:0, p2x:1, p2y:1, p3x:1, p3y:2}, {p1x:2, p1y:0, p2x:2, p2y:1, p3x:2, p3y:2} ];
+            return ( app.check.winCondition(piece, params[0]) || app.check.winCondition(piece, params[1]) || app.check.winCondition(piece, params[2]) );
+        },
+        
+        vertical: function(piece) {
+            var params = [ {p1x:0, p1y:0, p2x:1, p2y:0, p3x:2, p3y:0}, {p1x:0, p1y:1, p2x:1, p2y:1, p3x:2, p3y:1}, {p1x:0, p1y:2, p2x:1, p2y:2, p3x:2, p3y:2} ];
+            return ( app.check.winCondition(piece, params[0]) || app.check.winCondition(piece, params[1]) || app.check.winCondition(piece, params[2]) );
+        },
+        
+        diagonal: function(piece) {
+            var params = [ {p1x:0, p1y:0, p2x:1, p2y:1, p3x:2, p3y:2}, {p1x:0, p1y:2, p2x:1, p2y:1, p3x:2, p3y:0} ];
+            return ( app.check.winCondition(piece, params[0]) || app.check.winCondition(piece, params[1]) );
+        },
+        
+        allConditions: function() {
+            player1.win = (app.check.horizontal('X') || app.check.vertical('X') || app.check.diagonal('X'));
+            player2.win = (app.check.horizontal('O') || app.check.vertical('O') || app.check.diagonal('O'));
+        
+            if(player1.win || player2.win) {
+                alert(player1.win ? 'Player 1 Wins!' : 'Player 2 Wins!');
+                app.clearPieces();
+            }
+        }
+    }
 };
  
 $(function() {
